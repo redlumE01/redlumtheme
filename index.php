668bd46b3d46a65167e808e5e5be49cdd7886e6b
redlumE01 <info@redlum-media.com>
2018-07-17 20:00:40 +0100

    removed get page with rest api

--- index.php
+++ index.php
@@ -2,25 +2,7 @@
 
 <main id="main" class="site-main" role="main">
 	<article>
-		<div id="contenttarget"></div>
-			<?php $wpjsonurl = site_url() . '/wp-json/wp/v2/pages/'.get_the_id().''; ?>
-			<script type="text/javascript">
-				var xhttp = new XMLHttpRequest();
-				xhttp.onreadystatechange = function() {
-					if (this.readyState == 4 && this.status == 200) {
-						var obj = JSON.parse(this.response);
-						function html_entity_decode(message) {
-							var element = document.createElement('div');
-							element.innerHTML = message;
-							return element.innerHTML;
-						}
-						document.getElementById('contenttarget').innerHTML = html_entity_decode(obj.content.rendered);
-					}
-				};
-				xhttp.open('GET', '<?php echo $wpjsonurl ?>', true);
-				xhttp.send();
-			</script>
-
+		<?php the_content(); ?>
 	</article>
 </main>
 
