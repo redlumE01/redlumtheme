ff76de6cf664488673d086dddc0d8eaf287b42d6
Erik Mulder <info@redlum-media.com>
2020-01-19 14:20:47 +0100

    responsive embeds add / hint for using custom colors (needs to be included in stylesheets)

--- functions.php
+++ functions.php
@@ -58,10 +58,14 @@ function gutenberg_setup() {
 	// Enqueue editor styles.
 	add_editor_style( 'dist/gutenberg.css' );
 
+	// Responsive embeds (youtube, iframe)
+
+	add_theme_support( 'responsive-embeds' );
+
 	// Disable Custom Colors (color wheel)
 	//add_theme_support( 'disable-custom-colors' );
 
-	// Editor Color Palette
+	// Add Custom color palette + needed to created in stylesheet also (TODO)
 	//	add_theme_support( 'editor-color-palette', array(
 	//		array(
 	//			'name'  => __( 'Blue', 'ea-starter' ),
@@ -84,6 +88,101 @@ function gutenberg_setup() {
 	//			'color'	=> '#E2574C',
 	//		)
 	//	));
+
+	//add_filter( 'allowed_block_types', 'redlum_allowed_block_types' );
+
+	// 1. Common blocks category
+
+	//core/paragraph
+	//core/image
+	//core/heading
+	//core/gallery
+	//core/list
+	//core/quote
+	//core/audio
+	//core/cover
+	//core/file
+	//core/video
+
+	// 2. Formatting category
+
+	//core/table
+	//core/verse
+	//core/code
+	//core/freeform — Classic
+	//core/html — Custom HTML
+	//core/preformatted
+	//core/pullquote
+
+	// 3. Layout Elements category
+
+	//core/button
+	//core/text-columns — Columns
+	//core/media-text — Media and Text
+	//core/more
+	//core/nextpage — Page break
+	//core/separator
+	//core/spacer
+
+	// 4. Widgets category
+	//core/shortcode
+	//core/archives
+	//core/categories
+	//core/latest-comments
+	//core/latest-posts
+	//core/calendar
+	//core/rss
+	//core/search
+	//core/tag-cloud
+
+	// 5. Embeds category
+
+	//core/embed
+	//core-embed/twitter
+	//core-embed/youtube
+	//core-embed/facebook
+	//core-embed/instagram
+	//core-embed/wordpress
+	//core-embed/soundcloud
+	//core-embed/spotify
+	//core-embed/flickr
+	//core-embed/vimeo
+	//core-embed/animoto
+	//core-embed/cloudup
+	//core-embed/collegehumor
+	//core-embed/dailymotion
+	//core-embed/funnyordie
+	//core-embed/hulu
+	//core-embed/imgur
+	//core-embed/issuu
+	//core-embed/kickstarter
+	//core-embed/meetup-com
+	//core-embed/mixcloud
+	//core-embed/photobucket
+	//core-embed/polldaddy
+	//core-embed/reddit
+	//core-embed/reverbnation
+	//core-embed/screencast
+	//core-embed/scribd
+	//core-embed/slideshare
+	//core-embed/smugmug
+	//core-embed/speaker
+	//core-embed/ted
+	//core-embed/tumblr
+	//core-embed/videopress
+	//core-embed/wordpress-tv
+
+//	function redlum_allowed_block_types( $allowed_blocks ) {
+//		return array(
+//			'core/image',
+//			'core/paragraph',
+//			'core/heading',
+//			'core/list',
+//			'core/button',
+//			'core/columns',
+//			'core/spacer',
+//		);
+//	}
 
 }
 
